2f22142cf3f77e6517562a3c895c45d352936c16
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2024-07-30 19:58:32 -0300

    Update

--- Árvore_Binária/Main.java
+++ Árvore_Binária/Main.java
@@ -1,16 +1,18 @@
 import java.util.Random;
 
-public class Arvore {
+public class Main {
     public static void main(String[] args) {
         Random gerador = new Random();
         int numeroAleatorio = 0;
 
+        System.out.println("===========");
+
         for (int i = 0; i < 20; i++) {
             numeroAleatorio = gerador.nextInt(101);
-            System.out.println(numeroAleatorio);
+            System.out.println("    " + numeroAleatorio);
         }
 
-        System.out.println("======================================\n" + numeroAleatorio);
+        System.out.println("===========\n");
 
     }
 }
