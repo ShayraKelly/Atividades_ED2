dd767860971934448fff9c797c66198db8fbabb7
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2024-08-28 20:49:52 -0300

    Update

--- Árvore_Binária/ArvoreBinaria.java
+++ Árvore_Binária/ArvoreBinaria.java
@@ -26,6 +26,61 @@ public class ArvoreBinaria {
         }
     }
 
-    
+    //Pré-ordem
+    public void preOrdem(NoArvore no) {
+        if (no != null) {
+            System.out.print(no.getValor() + " ");
+            preOrdem(no.getNoEsq());
+            preOrdem(no.getNoDir());
+        }
+    }
 
+    //In-ordem
+    public void inOrdem(NoArvore no) {
+        if (no != null) {
+            inOrdem(no.getNoEsq());
+            System.out.print(no.getValor() + " ");
+            inOrdem(no.getNoDir());
+        }
+    }
+
+    //Pós-ordem
+    public void posOrdem(NoArvore no) {
+        if (no != null) {
+            posOrdem(no.getNoEsq());
+            posOrdem(no.getNoDir());
+            System.out.print(no.getValor() + " ");
+        }
+    }
+
+    public NoArvore remover(NoArvore no, int valor) {
+        if (no == null) {
+            return no;
+        }
+
+        if (valor < no.getValor()) {
+            no.setNoEsq(remover(no.getNoEsq(), valor));
+        } else if (valor > no.getValor()) {
+            no.setNoDir(remover(no.getNoDir(), valor));
+        } else {
+            if (no.getNoEsq() == null) {
+                return no.getNoDir();
+            } else if (no.getNoDir() == null) {
+                return no.getNoEsq();
+            }
+
+            NoArvore temp = minimo(no.getNoDir());
+            no.setValor(temp.getValor());
+            no.setNoDir(remover(no.getNoDir(), temp.getValor()));
+        }
+        return no;
+    }
+
+    private NoArvore minimo(NoArvore no) {
+        NoArvore atual = no;
+        while (atual.getNoEsq() != null) {
+            atual = atual.getNoEsq();
+        }
+        return atual;
+    }
 }
