382a95bebedbded5c159128a3ab1c222370e4f65
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2024-08-28 20:50:11 -0300

    Update

--- Árvore_Binária/Main.java
+++ Árvore_Binária/Main.java
@@ -14,5 +14,17 @@ public class Main {
 
         System.out.println("===========\n");
 
+    System.out.println("Pré-ordem:");
+    ArvoreBinaria.preOrdem(noarvore.getNoArvore());
+    System.out.println("In ordem:");
+    ArvoreBinaria.inOrdem(ArvoreBinaria.getNoArvore());
+    System.out.println("Pós-ordem:");
+    ArvoreBinaria.posOrdem(ArvoreBinaria.getNoArvore());
+
+    ArvoreBinaria.remover(NoArvore.getNoRaiz(), valor1);
+    ArvoreBinaria.remover(NoArvore.getNoRaiz(), valor2);
+
+    System.out.println("\nApós as remoções:");
+
     }
 }
